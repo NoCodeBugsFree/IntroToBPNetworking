1e80272f12a584bbdad15e9daa981656861818b3
i5 <i5@I5-PC>
2017-06-07 15:58:02 +0300

    Variable Replication (RepNotify)  - ReplicatedUsing = OnRep_

---
+++ Source/IntroToBPNetworking/StopLightRepNotify.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "StopLightRepNotify.h"
+
+
+// Sets default values
+AStopLightRepNotify::AStopLightRepNotify()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AStopLightRepNotify::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AStopLightRepNotify::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
