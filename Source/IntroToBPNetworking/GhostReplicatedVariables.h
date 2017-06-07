374a1a67b476d12483c8ed9b51ce9476c42c3f8c
i5 <i5@I5-PC>
2017-06-07 15:04:55 +0300

    1.3 Variable Replication

---
+++ Source/IntroToBPNetworking/GhostReplicatedVariables.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Ghost.h"
+#include "GhostReplicatedVariables.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class INTROTOBPNETWORKING_API AGhostReplicatedVariables : public AGhost
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
