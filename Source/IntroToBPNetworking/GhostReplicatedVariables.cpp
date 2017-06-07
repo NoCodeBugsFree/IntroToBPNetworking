153afcbffb4a08401fd0259e78080d509f9f71f2
i5 <i5@I5-PC>
2017-06-07 15:05:28 +0300

    1.3 Variable Replication

--- Source/IntroToBPNetworking/GhostReplicatedVariables.cpp
+++ Source/IntroToBPNetworking/GhostReplicatedVariables.cpp
@@ -1,7 +1,60 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "GhostReplicatedVariables.h"
+#include "Components/TextRenderComponent.h"
+#include "Net/UnrealNetwork.h"
 
+AGhostReplicatedVariables::AGhostReplicatedVariables()
+{
+	PrimaryActorTick.bCanEverTick = true;
 
+	// 
+	bReplicates = true;
+}
 
+void AGhostReplicatedVariables::BeginPlay()
+{
+	Super::BeginPlay();
 
+	// [Server] - Initialization and timer Server Only
+	if (Role == ROLE_Authority)
+	{	
+		InitializeHealth();
+		GetWorldTimerManager().SetTimer(HealthDecrementTimer, this, &AGhostReplicatedVariables::UpdateHealth, HealthDecrementTime, true);
+	}
+}
+
+void AGhostReplicatedVariables::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	
+	UpdateText();
+}
+
+void AGhostReplicatedVariables::UpdateHealth()
+{
+	Health -= HealthDecrementAmount;
+	if (Health <= 0)
+	{
+		InitializeHealth();
+	}
+	UpdateText();
+}
+
+void AGhostReplicatedVariables::InitializeHealth()
+{
+	Health = MaxHealth;
+}
+
+void AGhostReplicatedVariables::UpdateText()
+{
+	GhostText->SetText(FText::FromString(FString("Health: ") + FString::FromInt(Health)));
+}
+
+// GetLifetimeReplicatedProps
+void AGhostReplicatedVariables::GetLifetimeReplicatedProps(TArray<FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+
+	DOREPLIFETIME(AGhostReplicatedVariables, Health);
+}
--- Source/IntroToBPNetworking/GhostReplicatedVariables.h
+++ Source/IntroToBPNetworking/GhostReplicatedVariables.h
@@ -14,7 +14,41 @@ class INTROTOBPNETWORKING_API AGhostReplicatedVariables : public AGhost
 {
 	GENERATED_BODY()
 	
+public:
+
+	AGhostReplicatedVariables();
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaSeconds) override;
 	
+protected:
+
+	FTimerHandle HealthDecrementTimer;
+
+	/** Health Replicated/NonReplicated Variable  */
+	UPROPERTY(Replicated, VisibleAnywhere, BlueprintReadOnly, Category = "AAA", meta = (AllowPrivateAccess = "true"))
+	int32 Health = 0;
+
+	/** Maximum available health value  */
+	int32 MaxHealth = 100;
+
+	/** Health Decrement Amount per timer tick */
+	int32 HealthDecrementAmount = 1;
+
+	/** Timer tick interval  */
+	float HealthDecrementTime = 0.1;
+
+private:
+
+	/** Simple decrease health  */
+	void UpdateHealth();
+
+	/** Initialize Health  */
+	void InitializeHealth();
+
+	/** Update Text  */
+	void UpdateText();
 	
 	
 };
